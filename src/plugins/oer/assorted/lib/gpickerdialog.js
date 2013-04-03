80c10d8aa9ddc34a4332b5f427c2ff7322beb81a
gbenga <gbadipe@gmail.com>
2013-04-03 16:46:25 -0500

    Attempting to use iframe

--- src/plugins/oer/assorted/lib/gpickerdialog.js
+++ src/plugins/oer/assorted/lib/gpickerdialog.js
@@ -60,11 +60,11 @@ function VideoPickerCallback(data) {
         site_url = doc[google.picker.Document.URL]; // only shows website url :(
         // image_url = data.docs[0].thumbnails[data.docs[0].thumbnails.length - 1].url; // we only get a image preview with this
         embed_url = doc[google.picker.Document.EMBEDDABLE_URL];
-        var embed_code_template = Aloha.jQuery('<object width="420" height="236"><param name="movie" value="http://www.youtube.com/v/Rj8JoAAytyg?version=3&amp;hl=de_DE"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" frameborder="0"></embed></object>');
-        // var embed_code_template = Aloha.jQuery('<div class="multimedia-video"><iframe width="640" height="360" src="" wmode="transparent" frameborder="0" allowfullscreen></iframe></div>');
+        // var embed_code_template = Aloha.jQuery('<object width="420" height="236"><param name="movie" value="http://www.youtube.com/v/Rj8JoAAytyg?version=3&amp;hl=de_DE"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" frameborder="0"></embed></object>');
+        var embed_code_template = Aloha.jQuery('<div style="width:640; height=360" class="multimedia-video"><iframe width="640" height="360" src="" wmode="transparent" frameborder="0" allowfullscreen></iframe></div>');
         // var embed_code_template = Aloha.jQuery('<object width="420" height="360"><param name="movie" value="http://www.youtube.com/v/Rj8JoAAytyg?version=3&amp;hl=de_DE"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><iframe src="" type="application/x-shockwave-flash" width="560" height="360" allowscriptaccess="always" allowfullscreen="true" wmode="transparent" frameborder="0"></iframe></object>');
         //TODO Replace this to ensure only the specific video is changed
-        var embed_code = Aloha.jQuery(embed_code_template).find('embed').attr('src', embed_url);
+        var embed_code = Aloha.jQuery(embed_code_template).find('iframe').attr('src', embed_url);
         AlohaInsertIntoDom(embed_code);
     }
 }
