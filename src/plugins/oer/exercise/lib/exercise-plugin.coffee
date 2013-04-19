08dab7b30cbde48704b42da39df7cc48fded8bc0
Tom <woodwart@gmail.com>
2013-04-19 13:50:46 -0400

    flesh out some exercise plugin functionality

--- src/plugins/oer/exercise/lib/exercise-plugin.coffee
+++ src/plugins/oer/exercise/lib/exercise-plugin.coffee
@@ -8,25 +8,70 @@ define [
     '../../semanticblock/lib/semanticblock-plugin'], (Aloha, Plugin, jQuery, Ephemera, UI, Button, semanticBlock) ->
 
 	TEMPLATE = '''
-        <div class="note">
+        <div class="exercise">
             <div class="title-container dropdown">
-                <a class="type" data-toggle="dropdown">Note</a>
+                <a class="type" data-toggle="dropdown">Exercise</a>
                 <ul class="dropdown-menu">
-                    <li><a href="">Note</a></li>
-                    <li><a href="">Aside</a></li>
-                    <li><a href="">Warning</a></li>
-                    <li><a href="">Tip</a></li>
-                    <li><a href="">Important</a></li>
+                    <li><a href="">Exercise</a></li>
+                    <li><a href="">Homework</a></li>
+                    <li><a href="">Problem</a></li>
+                    <li><a href="">Question</a></li>
+                    <li><a href="">Task</a></li>
                 </ul>
                 <span class="title" semantic-editable placeholder="Add a title (optional)"></span>
             </div>
-            <div class="body" semantic-editable placeholder="Type the text of your note here."></div>
+            <div class="body" semantic-editable placeholder="Type the text of your exercise here."></div>
+            <div class="solution-placeholder">Click to add an Answer/Solution</div>
+            <div class="solution-controlls" style="display: none">
+                <a href="">[SHOW SOLUTION]</a>
+            </div>
         </div>
+	'''
+
+	SOLUTION_TEMPLATE = '''
+        <div class="solution">
+            <div class="title-container dropdown">
+                <a class="type" data-toggle="dropdown">Solution</a>
+                <ul class="dropdown-menu">
+                    <li><a href="">Solution</a></li>
+                    <li><a href="">Answer</a></li>
+                </ul>
+            </div>
+            <div class="body" semantic-editable placeholder="Type your solution here."></div>
+            <div class="solution-controlls">
+                <a href="">[HIDE SOLUTION]</a>
+            </div>
+        </div> 
 	'''
 
 	Plugin.create('exercise', {
 		init: () ->
 			semanticBlock.enableDragToAdd('Exercise', '[semantic-drag-source=exercise]', TEMPLATE)
+
+
+			semanticBlock.registerEvent('click', '.exercise .solution-placeholder', () ->
+				$(this).hide()
+				semanticBlock.appendElement($(SOLUTION_TEMPLATE), $(this).parent())
+			)
+
+			semanticBlock.registerEvent('click', '.exercise .semantic-delete', () ->
+				$(this).parents('.semantic-container').first().siblings('.solution-placeholder').removeAttr('style')
+			)
+
+			semanticBlock.registerEvent('click', '.exercise .solution-controlls a', (e) ->
+				e.preventDefault()
+				controlls = $(this).parent()
+				if (controlls.parent().is('.solution'))
+					container = controlls.parents('.semantic-container').first()
+					container.slideUp('slow', () ->
+						container.siblings('.solution-controlls').show()
+					)
+				else
+					controlls.hide()
+					controlls.siblings('.semantic-container').slideDown('slow')
+			)
+
+
 			UI.adopt 'insertExercise', Button,
 				click: (a, b, c) ->
 					semanticBlock.insertAtCursor(TEMPLATE)
--- src/plugins/oer/exercise/lib/exercise-plugin.js
+++ src/plugins/oer/exercise/lib/exercise-plugin.js
@@ -2,11 +2,33 @@
 (function() {
 
   define(['aloha', 'aloha/plugin', 'jquery', 'aloha/ephemera', 'ui/ui', 'ui/button', '../../semanticblock/lib/semanticblock-plugin'], function(Aloha, Plugin, jQuery, Ephemera, UI, Button, semanticBlock) {
-    var TEMPLATE;
-    TEMPLATE = '<div class="note">\n    <div class="title-container dropdown">\n        <a class="type" data-toggle="dropdown">Note</a>\n        <ul class="dropdown-menu">\n            <li><a href="">Note</a></li>\n            <li><a href="">Aside</a></li>\n            <li><a href="">Warning</a></li>\n            <li><a href="">Tip</a></li>\n            <li><a href="">Important</a></li>\n        </ul>\n        <span class="title" semantic-editable placeholder="Add a title (optional)"></span>\n    </div>\n    <div class="body" semantic-editable placeholder="Type the text of your note here."></div>\n</div>';
+    var SOLUTION_TEMPLATE, TEMPLATE;
+    TEMPLATE = '<div class="exercise">\n    <div class="title-container dropdown">\n        <a class="type" data-toggle="dropdown">Exercise</a>\n        <ul class="dropdown-menu">\n            <li><a href="">Exercise</a></li>\n            <li><a href="">Homework</a></li>\n            <li><a href="">Problem</a></li>\n            <li><a href="">Question</a></li>\n            <li><a href="">Task</a></li>\n        </ul>\n        <span class="title" semantic-editable placeholder="Add a title (optional)"></span>\n    </div>\n    <div class="body" semantic-editable placeholder="Type the text of your exercise here."></div>\n    <div class="solution-placeholder">Click to add an Answer/Solution</div>\n    <div class="solution-controlls" style="display: none">\n        <a href="">[SHOW SOLUTION]</a>\n    </div>\n</div>';
+    SOLUTION_TEMPLATE = '<div class="solution">\n    <div class="title-container dropdown">\n        <a class="type" data-toggle="dropdown">Solution</a>\n        <ul class="dropdown-menu">\n            <li><a href="">Solution</a></li>\n            <li><a href="">Answer</a></li>\n        </ul>\n    </div>\n    <div class="body" semantic-editable placeholder="Type your solution here."></div>\n    <div class="solution-controlls">\n        <a href="">[HIDE SOLUTION]</a>\n    </div>\n</div> ';
     return Plugin.create('exercise', {
       init: function() {
         semanticBlock.enableDragToAdd('Exercise', '[semantic-drag-source=exercise]', TEMPLATE);
+        semanticBlock.registerEvent('click', '.exercise .solution-placeholder', function() {
+          $(this).hide();
+          return semanticBlock.appendElement($(SOLUTION_TEMPLATE), $(this).parent());
+        });
+        semanticBlock.registerEvent('click', '.exercise .semantic-delete', function() {
+          return $(this).parents('.semantic-container').first().siblings('.solution-placeholder').removeAttr('style');
+        });
+        semanticBlock.registerEvent('click', '.exercise .solution-controlls a', function(e) {
+          var container, controlls;
+          e.preventDefault();
+          controlls = $(this).parent();
+          if (controlls.parent().is('.solution')) {
+            container = controlls.parents('.semantic-container').first();
+            return container.slideUp('slow', function() {
+              return container.siblings('.solution-controlls').show();
+            });
+          } else {
+            controlls.hide();
+            return controlls.siblings('.semantic-container').slideDown('slow');
+          }
+        });
         return UI.adopt('insertExercise', Button, {
           click: function(a, b, c) {
             return semanticBlock.insertAtCursor(TEMPLATE);
