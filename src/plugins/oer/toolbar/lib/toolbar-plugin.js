6977920c965c79fcc47daef21b64448d3ba1252c
Izak Burger <isburger@gmail.com>
2013-05-07 00:27:01 +0200

    don't let mousedown on actions propagate
    This prevents a click on the toolbar from deactivating the current
    editor.

--- src/plugins/oer/toolbar/lib/toolbar-plugin.js
+++ src/plugins/oer/toolbar/lib/toolbar-plugin.js
@@ -151,6 +151,9 @@
           return $newEl.bind('click', headingFunc);
         };
         $ROOT.on('click', '.action.changeHeading', changeHeading);
+        $ROOT.on('mousedown', ".action", function(evt) {
+          return evt.stopPropagation();
+        });
         Aloha.bind('aloha-editable-activated', function(event, data) {
           return squirreledEditable = data.editable;
         });
