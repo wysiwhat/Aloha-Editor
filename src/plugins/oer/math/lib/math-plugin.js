3eb0a8ee320b90732c887e623c1ecc7122b97d9a
Izak Burger <isburger@gmail.com>
2013-05-31 15:36:36 +0200

    Alter height of editable when opening cheatsheet
    This allows you to scroll the popover into view if you're editing math
    towards the bottom of the page, and the popover is covered up by the
    cheatsheet.

--- src/plugins/oer/math/lib/math-plugin.js
+++ src/plugins/oer/math/lib/math-plugin.js
@@ -406,9 +406,20 @@
     opener = help.find('.cheatsheet-open');
     help.on('show', function(e) {
       opener.hide();
-      return jQuery(this).find('.cheatsheet').slideDown("fast");
+      return jQuery(this).find('.cheatsheet').slideDown("fast", function() {
+        var a, h;
+
+        if (Aloha.activeEditable) {
+          a = jQuery(this).height();
+          h = Aloha.activeEditable.obj.height();
+          return h = Aloha.activeEditable.obj.height(a + h + 50);
+        }
+      });
     });
     help.on('hide', function(e) {
+      if (Aloha.activeEditable) {
+        Aloha.activeEditable.obj.height('auto');
+      }
       return jQuery(this).find('.cheatsheet').slideUp("fast", function() {
         return opener.show();
       });
